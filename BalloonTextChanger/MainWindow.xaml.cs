086643ec2dfd1183eedfb776bc120a276939383c
thieupepijn <thieupepijn@hotmail.com>
2020-05-15 00:15:19 +0200

    third commit

--- BalloonTextChanger/MainWindow.xaml.cs
+++ BalloonTextChanger/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Brushes = System.Windows.Media.Brushes;
+using Color = System.Drawing.Color;
+using Point = System.Windows.Point;
 
 namespace BalloonTextChanger
 {
@@ -21,6 +26,8 @@ namespace BalloonTextChanger
     /// </summary>
     public partial class MainWindow : Window
     {
+        Bitmap _bitmap;
+      
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +41,9 @@ namespace BalloonTextChanger
             {
                 Uri uri = new Uri(dialog.FileName);
                 BitmapImage image = new BitmapImage(uri);
+                _bitmap = BitmapImage2Bitmap(image);
+
+
                 mainCanvas.Background = new ImageBrush(image);
 
                 mainCanvas.Width = image.Width;
@@ -46,15 +56,58 @@ namespace BalloonTextChanger
         {
             Point coord = e.GetPosition(mainCanvas); ;
 
-            Ellipse cirkel = new Ellipse();
-            cirkel.Fill = Brushes.Red;
-            cirkel.Width = 5;
-            cirkel.Height = 5;
+            //Ellipse cirkel = new Ellipse();
+            //cirkel.Fill = Brushes.Red;
+            //cirkel.Width = 5;
+            //cirkel.Height = 5;
 
-            Canvas.SetLeft(cirkel, coord.X);
-            Canvas.SetTop(cirkel, coord.Y);
-            mainCanvas.Children.Add(cirkel);
+            //Canvas.SetLeft(cirkel, coord.X);
+            //Canvas.SetTop(cirkel, coord.Y);
+            //mainCanvas.Children.Add(cirkel);
 
+            Color color = GetColor(_bitmap, coord);
+            if (IsWhite(color))
+            {
+                MessageBox.Show("Wit!");
+            }
+            else
+            {
+                MessageBox.Show("Niet-Wit!");
+            }
         }
+
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+
+        private Color GetColor(Bitmap bitmap, Point coord)
+        {
+           Color color = bitmap.GetPixel((int)coord.X, (int)coord.Y);
+            return color;
+        }
+
+        private bool IsWhite(Color color)
+        {
+            if ((color.R < 200) || (color.G < 200) || (color.B < 200))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
     }
 }
